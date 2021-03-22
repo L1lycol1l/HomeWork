6e7ad5d0a2ac5b0f96fbe66806c302aaca2097bb
L1lycol1l <80588442+L1lycol1l@users.noreply.github.com>
2021-03-22 11:35:30 +0300

    Add files via upload

---
+++ src/hwork2/HW6.java
@@ -0,0 +1,15 @@
+package hwork2;
+
+import java.util.Scanner;
+
+public class HW6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Загадали число?");
+        int question1 = in.nextInt();
+        int number = 1;
+
+    }
+}
+
+//System.out.println("Это " + a / 2 + " Угадал?");
