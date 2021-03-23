9d40d61f8765792a4f09b9fd2ab01b69cea28c39
L1lycol1l <80588442+L1lycol1l@users.noreply.github.com>
2021-03-23 23:32:17 +0300

    Delete src/src directory

--- src/src/hwork1/HW1.java
+++
@@ -1,9 +0,0 @@
-package hwork1;
-
-public class HW1 {
-    public static void main (String[] args){
-        long start=4, end=9;
-        double f = start < end ? end-start : -1;
-        System.out.println(f);
-    }
-}
